c882e0b14d9f49f5d4c991fc9fe51b085ca2dc17
Pratik Sasmal <pratiksasmaljedi@gmail.com>
2020-11-12 12:42:52 +0530

    adding user profile

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
 	content = models.TextField()
 	date_posted = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
-	#if user deletes id then psot is deleted
+	#if user deletes id then post is deleted
 
 	def __str__(self):
 		return self.title
--- django_app/settings.py
+++ django_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,5 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')#directory where uploaded files willbe saved
+MEDIA_URL = '/media/'#public url of directory
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'#name given to url pattern
