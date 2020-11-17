e7f9d59df0479aa0f164a2b3bc6b9a09ee4bd971
Pratik Sasmal <pratiksasmaljedi@gmail.com>
2020-11-17 20:23:13 +0530

    addinga feature so that user can create, update and delete the post

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
-from .views import PostListView , PostDetailView
+from .views import (
+	PostListView,
+	PostDetailView,
+	PostCreateView,
+	PostUpdateView,
+	PostDeleteView
+)
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(),name='blog-home'),#home route url
-    path('post/<int:pk>', PostDetailView.as_view(),name='blog-detail'),#id of post is a prt of route
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),#id of post is a prt of route
+    path('post/new', PostCreateView.as_view(),name='blog-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about,name='blog-about'),
+
 ]
 
 # classes are looking for <app>/<model>_<viewtype>.html
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 
 def home(request):
@@ -18,6 +19,42 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
 	model = Post
+
+class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):#create new posts
+	model = Post
+	fields = ['title', 'content']
+	#userpassestestmixin checks for certain test conditions b4 allowing edits
+	#overwriting the form valid method
+
+	def form_valid(self, form): #overriding form valid method
+		form.instance.author = self.request.user
+		return super().form_valid(form) #runnning on parent class
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):#create new posts
+	model = Post
+	fields = ['title', 'content']
+	#overwriting the form valid method
+
+	def form_valid(self, form): #overriding form valid method
+		form.instance.author = self.request.user
+		return super().form_valid(form) #runnning on parent class 
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Post
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
 
 #using functon
 def about(request):
